19614d6d10e1fdeeda0105315cf29746ca46247d
Jacob Roshgadol <jacobiroshgadol@gmail.com>
2017-12-27 21:53:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,6 +35,6 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  array = array.slice(-1)
+  array = array.pop()
   return array
 }
